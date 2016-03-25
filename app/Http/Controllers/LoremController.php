23cd13e8256e7cae17181887774f9edc28b0a109
brld <brandon.lee.darby@gmail.com>
2016-03-25 15:36:15 -0700

    Created two new views--this backup serves as a version WITHOUT views, confirmed to work

--- app/Http/Controllers/LoremController.php
+++ app/Http/Controllers/LoremController.php
@@ -2,8 +2,8 @@
 
 namespace P3\Http\Controllers;
 
-require_once '/path/to/Faker/src/autoload.php';
 use P3\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class LoremController extends Controller {
     public function getLorem() {
@@ -28,18 +28,48 @@ class LoremController extends Controller {
     public function getUser() {
       $view  = '<form method="POST" action="/user-generator">';
       $view .= csrf_field();
-      $view .= 'Number of people: <input type="text" name="title">';
+      $view .= 'Number of people: <input type="text" name="title" value="5">';
+      $view .= 'Include Address? <input type="checkbox" name="address">';
+      $view .= 'Include Company? <input type="checkbox" name="company">';
+      $view .= 'Include Phone number? <input type="checkbox" name="phonenumber">';
+      $view .= 'Include Age? <input type="checkbox" name="age">';
       $view .= '<input type="submit">';
       $view .= '</form>';
 
       return $view;
     }
 
-    public function postUser() {
-      $faker = Faker\Factory::create();
+    public function postUser(Request $request) {
+      $faker = \Faker\Factory::create();
+      $numPeople = $request->input('title');
+      $includeAddress = $request->input('address');
+      $includeCompany = $request->input('company');
+      $includePhoneNumber = $request->input('phonenumber');
+      $includeAge = $request->input('age');
       for ($i=0; $i < $numPeople; $i++) {
         echo $faker->name;
-        echo $faker->address;
+        if ($includeAddress == TRUE || $includeCompany == TRUE || $includePhoneNumber == TRUE || $includeAge == TRUE) {
+          echo (",");
+        }
+        echo (" ");
+        if ($includeAddress == TRUE) {
+          echo $faker->address;
+          echo (", ");
+        }
+        if ($includeCompany == TRUE) {
+          echo $faker->company;
+          echo (", ");
+        }
+        if ($includePhoneNumber == TRUE) {
+          echo $faker->phoneNumber;
+          echo (", ");
+        }
+        if ($includeAge == TRUE) {
+          echo $faker->numberBetween($min = 20, $max = 80);
+          echo (", ");
+        }
+
+        echo nl2br ("\n \n");
       }
 
 
