cbb7ee3d9c9f894068965643a970e315b7428b44
brld <brandon.lee.darby@gmail.com>
2016-03-27 01:35:24 -0700

    Design changes

--- resources/views/userpost.blade.php
+++ resources/views/userpost.blade.php
@@ -10,27 +10,21 @@
 @for ($i=0; $i < $numPeople; $i++)
   <?php require_once '../vendor/fzaninotto/faker/src/autoload.php'; ?>
   <p class="pname">
-    <?php
-    echo $faker->name;
-    if ($includeAddress == TRUE || $includeCompany == TRUE || $includePhoneNumber == TRUE || $includeAge == TRUE) {
-      echo (",");
-    }
-    ?>
+    <?php echo $faker->name; ?>
   </p>
   <p>
   <?php
-  if ($includeAge == TRUE) {
-    echo $faker->numberBetween($min = 20, $max = 80)." years old<br>";
-  }
-  echo (" ");
   if ($includeAddress == TRUE) {
     echo $faker->address()."<br>";
+  }
+  if ($includePhoneNumber == TRUE) {
+    echo $faker->phoneNumber()."<br>";
   }
   if ($includeCompany == TRUE) {
     echo "has a job at ".$faker->company()."<br>";
   }
-  if ($includePhoneNumber == TRUE) {
-    echo $faker->phoneNumber();
+  if ($includeAge == TRUE) {
+    echo $faker->numberBetween($min = 20, $max = 80)." years old<br>";
   }
 
   ?>
